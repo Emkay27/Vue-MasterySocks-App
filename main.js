a66d187a5bd5068e57047e5404347cd3d0db8a54
Kgatliso Mkhwanazi <kgatliso@synthesis.co.za>
2022-03-02 20:24:31 +0200

    Added a pic of socks and also added a link

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() { 
         return {
             product: 'Socks',
-            description: 'These are beautiful socks' 
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.mdpi.com/2073-8994/14/1/108' 
         }
     }
 })
