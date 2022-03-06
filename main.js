58dafc5231c1a57328ae1c9eb6fbdd347c78752f
Kgatliso Mkhwanazi <kgatliso@synthesis.co.za>
2022-03-06 16:14:41 +0200

    List rendering, added details, variants and sizes to our product in the app

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             Inventory: 100,
             onSale: true,
-            url: 'https://www.mdpi.com/2073-8994/14/1/108' 
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [{id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}],
+            sizes: ['S', 'M', 'L', 'XL']
         }
     }
 })
